62856fb3ad557b4a66d04f683c697ee9e595ebb5
unknown <ed.f.jones@gmail.com>
2020-05-27 02:01:15 -0400

    finished prev btn and next btn and progress bar

--- script.js
+++ script.js
@@ -46,6 +46,44 @@ function pauseSong() {
     audio.pause();
 }
 
+// previous song
+function prevSong(){
+    songIndex--;
+
+    if(songIndex < 0) {
+        songIndex = songs.length -1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// next song
+function nextSong(){
+    songIndex++;
+
+    if(songIndex > songs.length - 1 ) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// update progress bar
+function updateProgress(e){
+const {duration, currentTime} = e.srcElement;
+const progressPercent = (currentTime / duration) * 100;
+progress.style.width = `${progressPercent}%`;
+}
+
+// set progress bar
+function setProgress(e){
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
+}
+
 // event listeners
 playBtn.addEventListener('click', ()=> {
 const isPlaying = musicContainer.classList.contains('play');
@@ -56,3 +94,16 @@ if (isPlaying) {
     playSong();
 }
 });
+
+// change songs
+prevBtn.addEventListener('click',prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// progess bar 
+audio.addEventListener('timeupdate', updateProgress);
+
+// click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+//song ends
+audio.addEventListener('ended', nextSong);
